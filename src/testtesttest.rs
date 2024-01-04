d27533c9a17c770042f654d85de4a41c41597d6e
Christian <christian.schneiderwind@tu-ilmenau.de>
2024-01-04 22:01:44 +0100

    new improved ISM template started!

--- src/testtesttest.rs
+++ src/testtesttest.rs
@@ -1,83 +1,145 @@
-use nalgebra::{Point, Quaternion, Point3};
-
 use crate::buffers::CircularDelayBuffer;
+use nalgebra::Point3;
+use nalgebra::Quaternion;
 
-use indextree::Arena;
 
-
-#[derive(Debug)]
-struct Source {
-   pub position: Point3<f32>,
-   pub orientation: Quaternion<f32>,
-   pub buffer: CircularDelayBuffer,
-   pub dist: f32,
+#[derive(Debug, Default)]
+pub enum Reflected {
+    X0, X1,
+    Y0, Y1,
+    Z0, Z1,
+    #[default]
+    No,
 }
 
+
+#[allow(dead_code)]
 #[derive(Debug)]
+struct Room {
+    dimension: Point3<f32>,
+}
+
+#[allow(dead_code)]
+impl Room {
+    fn new(width: f32, height: f32, length: f32) -> Self {
+        Self {
+            dimension: Point3::new(width, height, length),
+        }
+    }
+    fn diagonal(&self) -> f32 {
+        (self.dimension.x.powi(2) + self.dimension.y.powi(2) + self.dimension.z.powi(2)).sqrt()
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+struct Source {
+    pub position: Point3<f32>,
+    pub orientation: Quaternion<f32>,
+    pub buffer: CircularDelayBuffer,
+    pub dist: f32,
+    pub Reflected,
+}
+
+#[allow(dead_code)]
+impl Source {
+    pub fn new(
+        position: Point3<f32>,
+        orientation: Quaternion<f32>,
+        room: &Room,
+        speed_of_sound: f32,
+        sample_rate: f32,
+        list: Option<Listener>,
+    ) -> Self {
+        let dist = if let Some(x) = list {
+            nalgebra::distance(&x.position, &position)
+        } else {
+            0.0
+        };
+        Self {
+            position,
+            orientation,
+            buffer: CircularDelayBuffer::new(
+                (sample_rate * room.diagonal() / speed_of_sound).ceil() as usize,
+            ),
+            dist,
+        }
+    }
+    pub fn update_position(&mut self, position: Point3<f32>, listener: &Listener) {
+        self.position = position;
+        self.dist = nalgebra::distance(&self.position, &listener.position);
+        self.buffer
+            .set_delay_time_samples(48000.0 * self.dist / 343.0f32);
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Default)]
 struct Listener {
-   pub position: Point3<f32>,
-   pub orientation: Quaternion<f32>,
+    pub position: Point3<f32>,
+    pub orientation: Quaternion<f32>,
 }
 
 #[cfg(test)]
 #[test]
-
-fn test1() { 
-
+fn test_bufs() {
     // Init things
 
-    use nalgebra::OPoint;
+    use indextree::Arena;
+    use nalgebra::Point3;
     use num_traits::Zero;
 
     // use Listener;
     let speed_of_sound: f32 = 343.0;
     let sample_rate: f32 = 48000.0;
     let room: [f32; 3] = [5.0, 3.0, 6.0];
-    let max_delay = (sample_rate  
-                       * (room.iter().fold(0.0,|acc, x| acc+x.powi(2))).sqrt()  
-                       / speed_of_sound).round() as usize + 200;    
+    let max_delay = (sample_rate * (room.iter().fold(0.0, |acc, x| acc + x.powi(2))).sqrt()
+        / speed_of_sound)
+        .round() as usize
+        + 200;
     let mut lis = Listener {
         position: Point3::default(),
         orientation: Quaternion::zero(),
     };
 
-
     let mut s1 = Source {
-        position: Point3::default(), 
+        position: Point3::default(),
         orientation: Quaternion::default(),
         buffer: CircularDelayBuffer::new(max_delay),
-        dist: 10.0,
+        dist: 0.0,
     };
 
     let mut s1_is_a = Source {
-        position: Point3::default(), 
+        position: Point3::default(),
         orientation: Quaternion::default(),
         buffer: CircularDelayBuffer::new(max_delay),
-        dist: 20.0,
+        dist: 0.0,
     };
 
     let s1_is_ab = Source {
-        position: Point3::default(), 
+        position: Point3::default(),
         orientation: Quaternion::default(),
         buffer: CircularDelayBuffer::new(max_delay),
-        dist: 30.0,
+        dist: 0.0,
     };
 
     // update position
     lis.position = Point3::new(1.0, 1.5, 2.0);
     s1.position = Point3::new(2.0, 1.5, 4.0);
-    s1.dist = nalgebra::distance(&lis.position,&s1.position);
-    s1.buffer.set_delay_time_samples(sample_rate * s1.dist / 343.0);
+    s1.dist = nalgebra::distance(&lis.position, &s1.position);
+    s1.buffer
+        .set_delay_time_samples(sample_rate * s1.dist / 343.0);
     s1_is_a.position = Point3::new(-2.0, 1.5, 2.0);
     s1_is_a.dist = nalgebra::distance(&lis.position, &s1_is_a.position);
-    s1_is_a.buffer.set_delay_time_samples(sample_rate * (s1_is_a.dist-s1.dist) / 343.0);
+    s1_is_a
+        .buffer
+        .set_delay_time_samples(sample_rate * (s1_is_a.dist - s1.dist) / 343.0);
 
     let out: Vec<f32> = vec![0.0; 48000];
     let mut input: Vec<f32> = out;
     input[0] = 1.0;
     let mut temp = Vec::new();
-    for i in 0 .. input.len() {
-    
+    for i in 0..input.len() {
         let mut sout = 0.0;
         let sample = input[i];
         sout = s1.buffer.read();
@@ -87,26 +149,22 @@ fn test1() {
         sout += s1_out;
 
         if sout > 0.0 {
-            temp.push( (i as f32) );
+            temp.push(i as f32);
         }
         // print!("{:?}|", sout);
-        
     }
     println!("Pulses found at: {:?}", temp);
-    let arena =  &mut Arena::new();
+    let arena = &mut Arena::new();
     let mut node_ids = Vec::new();
 
     let root = arena.new_node(s1);
     node_ids.push(arena.new_node(s1_is_a));
     node_ids.push(arena.new_node(s1_is_ab));
-  
+
     root.append(node_ids[0], arena);
     node_ids[0].append(node_ids[1], arena);
 
     //root.children(arena).for_each(|x| {dbg!(arena.get(x));});
 
-    // 
-    
-    
-    
+    //
 }
