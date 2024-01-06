5f41b86b629f621a3352e7f04dac6c792899213c
Vibrrra <christian.schneiderwind@gmx.de>
2024-01-06 16:56:49 +0100

    Working ISM Tree added

--- src/scene_parser.rs
+++ src/scene_parser.rs
@@ -1,5 +1,10 @@
 use crate::audioSceneHandlerData::Scene_data;
+use crate::testtesttest::{self, AudioSceneHandle};
 
-pub fn parse_scene(scene: &Scene_data) {
+pub fn update_scene(scene: &Scene_data, audio_scene_handle: &mut AudioSceneHandle) {
+    
+    audio_scene_handle.sources.iter_mut().zip(scene.sources.iter()).for_each(|(ashs, ss)| { 
+
+    });
     unimplemented!();
 }
--- src/testtesttest.rs
+++ src/testtesttest.rs
@@ -1,6 +1,10 @@
+use crate::audioSceneHandlerData::Scene_data;
+use crate::audioSceneHandlerData::Transform;
 use crate::buffers::CircularDelayBuffer;
+use indextree::NodeId;
 use nalgebra::Vector3;
 use nalgebra::Quaternion;
+use nalgebra::zero;
 use num_traits::Zero;
 use indextree::Arena;
 use strum_macros::EnumIter;
@@ -73,7 +77,7 @@ impl Source {
         speed_of_sound: f32,
         sample_rate: f32,
         reflector: Option<Reflected>,
-        list: Option<Listener>,        
+        list: Option<&Listener>,        
     ) -> Self {
         let mut source_listener_orientation = Quaternion::zero();
         let dist = if let Some(x) = list {
@@ -129,23 +133,119 @@ impl ListenerSourceView {
     }
 }
 
-struct AudioSceneHandle {
-    sources: Vec<Source>,
-    listenerView: Vec<ListenerSourceView>,
-    ims_order: usize,
+pub struct AudioSceneHandle {
+    pub sources: Vec<Source>,
+    pub listenerView: Vec<ListenerSourceView>,
+    pub ims_order: usize,
     
 }
 
+impl AudioSceneHandle {
+    pub fn update_source_vec(&mut self, pb_scene_data: &Scene_data) {
+        let pbs_transforms = &pb_scene_data.sources.transforms;
+        assert!(self.sources.len() != pbs_transforms.len()); //pb_scene_data.sources.0.unwrap().transforms.len());
+        for n in 0..self.sources.len() {
+            for order in 0..self.ims_order {
+
+            }
+            unimplemented!()
+        }
+    }
+
+}
+
+pub struct SourceTrees {
+    pub arenas: Vec<Arena<Source>>,
+    node_lists: Vec<Vec<NodeId>>,
+    pub roots: Vec<NodeId>
+}
+
+pub fn create_source_tree(sources: Vec<Source>, room: &Room, ism_order: usize) -> SourceTrees {//(Arena<Source>, Vec<NodeId>) {
+    
+    let offset = is_per_model(ism_order, 6);
+    let mut node_lists = Vec::new();
+    let mut source_tree_vec = Vec::new(); 
+    let mut roots = Vec::new(); 
+    for n in 0 .. sources.len() {
+        let mut arena = indextree::Arena::new();
+        let mut node_list = Vec::new();
+        let new_node = arena.new_node(sources[n].clone());
+        node_list.push(new_node); 
+        roots.push(new_node);
+        
+        for order in 0..ism_order {
+            // for i in N_IS_INDEX_RANGES[order].0+n*(offset+1) .. N_IS_INDEX_RANGES[order].1+n*(offset+1) {
+            for i in N_IS_INDEX_RANGES[order].0 .. N_IS_INDEX_RANGES[order].1 {
+                for boundary in Reflected::VALUES {
+                    
+                    // let current_node = arena.get(node_list[i]).unwrap();
+                    let current_source = arena.get(node_list[i]).unwrap().get();
+                    
+                    if current_source.reflector != boundary  {
+                        let new_node = arena.new_node(create_ism(current_source, room, &boundary));
+                        node_list[i].append(new_node, &mut arena);
+                        node_list.push(new_node);
+                        
+                    }
+                }
+            }
+        }
+        source_tree_vec.push(arena);
+        node_lists.push(node_list);
+    }
+    let source_tree = SourceTrees{arenas: source_tree_vec, node_lists, roots};
+    source_tree
+    //(arena, node_list)
+}
+
+pub fn update_source_tree(source_trees: &mut SourceTrees, sources: Vec<Source>, room: &Room, ism_order: usize) {
+    let offset = is_per_model(ism_order, 6);
+
+    for (n, new_source) in sources.iter().enumerate() {
+        for i in 0 .. source_trees.arenas[n].count() {
+            let parent_node = source_trees.arenas[n].get(source_trees.node_lists[n][i]).unwrap().parent();
+            match parent_node {
+                Some(pn) => {
+                    let parent_position: nalgebra::Matrix<f32, nalgebra::Const<3>, nalgebra::Const<1>, nalgebra::ArrayStorage<f32, 3, 1>> = 
+                        source_trees.arenas[n].get(pn).unwrap().get().position;
+                    let current_node: &mut Source = source_trees.arenas[n].get_mut(source_trees.node_lists[n][i]).unwrap().get_mut();
+                    *current_node.position = *calc_ism_position(&parent_position, room, &current_node.reflector);
+                },
+                None => {
+                    let data = source_trees.arenas[n].get_mut(source_trees.node_lists[n][i]).unwrap().get_mut();
+                    *data.position = *new_source.position; 
+
+                },
+            }
+        }
+    
+    }
+    // unimplemented!()
+    // for n in 0 .. sources.len() {        
+    //     for order in 0..ism_order {
+    //         for i in N_IS_INDEX_RANGES[order].0+n*(offset+1) .. N_IS_INDEX_RANGES[order].1+n*(offset+1) {
+    //             for boundary in Reflected::VALUES {
+    //                 let current_source = source_tree.arena.get(source_tree.node_list[i]).unwrap().get();
+    //                 if current_source.reflector != boundary  {
+    //                     let mut_node = source_tree.arena.get_mut(source_tree.node_list[i]).unwrap().get_mut(); 
+    //                     mut_node.position = calc_ism_position(current_source, room, &boundary);
+                        
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+}
 // calc ISMs
-pub fn generate_image_source_tree(sources: Vec<Source>, room: &Room, ism_order: usize) -> Vec<Source> {
+pub fn generate_image_source_vec(sources: Vec<Source>, room: &Room, ism_order: usize) -> Vec<Source> {
     
     // create Tree Arena
-    let mut arena: Arena<&Source> = Arena::new();
-    let mut root_nodes: Vec<indextree::NodeId> = Vec::new();
+    //let mut arena: Arena<&Source> = Arena::new();
+    //let mut root_nodes: Vec<indextree::NodeId> = Vec::new();
     // let mut node_list: Vec<indextree::NodeId> = Vec::new();
     let mut source_list: Vec<Source> = Vec::new();
     let mut current_ism_order: usize = 1;
-    let root_offset = 
+    //let root_offset = 
     // for all n real sources
     for n in 0..sources.len() {
         
@@ -158,30 +258,30 @@ pub fn generate_image_source_tree(sources: Vec<Source>, room: &Room, ism_order: 
                 
                 for boundary in Reflected::VALUES {
                     if source_list[i].reflector != boundary {
-                        source_list.push(create_reflection(&source_list[i], room, &boundary));
+                        source_list.push(create_ism(&source_list[i], room, &boundary));
                     }
                 }
             }
         }
-    };
-    
-    source_list
+    };    source_list
 }
+
 
 // This function creates an image source from reflecting a source
 // on an respective room boundary
-fn create_reflection(s: &Source, r: &Room, b: &Reflected) -> Source {   
+fn create_ism(s: &Source, r: &Room, b: &Reflected) -> Source {   
     
-    let mut position = s.position;
-    match b {
-        Reflected::X0 => {position.x = -position.x},
-        Reflected::X1 => {position.x = 2.0 * r.dimension.x - position.x},
-        Reflected::Y0 => {position.y = -position.y},
-        Reflected::Y1 => {position.y = 2.0 * r.dimension.y - position.y},
-        Reflected::Z0 => {position.z = -position.z},
-        Reflected::Z1 => {position.z = 2.0 * r.dimension.z - position.z},
-        Reflected::No => {panic!("This should not happen!")},
-    } 
+    let position = calc_ism_position(&s.position, r, b);
+    // let mut position = s.position;
+    // match b {
+    //     Reflected::X0 => {position.x = -position.x},
+    //     Reflected::X1 => {position.x = 2.0 * r.dimension.x - position.x},
+    //     Reflected::Y0 => {position.y = -position.y},
+    //     Reflected::Y1 => {position.y = 2.0 * r.dimension.y - position.y},
+    //     Reflected::Z0 => {position.z = -position.z},
+    //     Reflected::Z1 => {position.z = 2.0 * r.dimension.z - position.z},
+    //     Reflected::No => {panic!("This should not happen!")},
+    // } 
     Source::new(
         position, 
         Quaternion::zero(), 
@@ -191,6 +291,36 @@ fn create_reflection(s: &Source, r: &Room, b: &Reflected) -> Source {
         Some(b.clone()), None
     )
 }
+// This function creates an image source from reflecting a source
+// on an respective room boundary
+fn calc_ism_position(source_position: &Vector3<f32>, r: &Room, b: &Reflected) -> Vector3<f32> {   
+    
+    let mut position = *source_position;
+    match b {
+        Reflected::X0 => {position.x = -position.x},
+        Reflected::X1 => {position.x = 2.0 * r.dimension.x - position.x},
+        Reflected::Y0 => {position.y = -position.y},
+        Reflected::Y1 => {position.y = 2.0 * r.dimension.y - position.y},
+        Reflected::Z0 => {position.z = -position.z},
+        Reflected::Z1 => {position.z = 2.0 * r.dimension.z - position.z},
+        Reflected::No => {panic!("This should not happen!")},
+    } 
+    position
+}
+
+// helper functions
+fn is_per_model(maxorder: usize, n_surfaces: usize) -> usize {
+    let mut n_ism: usize = 0;
+    for i in 1..=maxorder {
+        n_ism += is_per_order(i as f64, n_surfaces as f64) as usize;
+    }
+    n_ism
+}
+
+fn is_per_order(order: f64, n_surfaces: f64) -> usize {
+    ((n_surfaces) * (n_surfaces - 1f64).powf(order - 1f64)).floor() as usize
+}
+
 
 #[cfg(test)]
 #[test]
@@ -296,17 +426,56 @@ fn calc_distance(v1: &Vector3<f32>, v2: &Vector3<f32>) -> f32 {
 
 #[test]
 fn test_ism_creation() {
-    let ism_order: usize = 3;
+    let ism_order: usize = 1;
     let speed_of_sound: f32 = 343.0;
     let sample_rate: f32 = 48000.0;
     let room: Room = Room::new(4.0, 3.0, 5.0);
     let listener = Listener::default();
-    let ssrc: Source = Source::new(Vector3::new(2.0, 1.0, 2.0), Quaternion::zero(), &room, speed_of_sound, sample_rate, None, Some(listener));
+    let ssrc: Source = Source::new(Vector3::new(2.0, 1.0, 2.0), Quaternion::zero(), &room, speed_of_sound, sample_rate, None, Some(&listener));
 
-    let src_list = generate_image_source_tree(vec![ssrc], &room, ism_order);
+    let src_list = generate_image_source_vec(vec![ssrc], &room, ism_order);
 
-    for i in src_list {
-        println!("{:?}", i.position);
+    for i in src_list.iter().enumerate() {
+        println!("Nr.: {}, {:?}", i.0, i.1.position);
+    }
+}
+#[test]
+fn test_ism_tree_creation() {
+    let ism_order: usize = 1;
+    let speed_of_sound: f32 = 343.0;
+    let sample_rate: f32 = 48000.0;
+    let room: Room = Room::new(4.0, 3.0, 5.0);
+    let listener = Listener::default();
+    let ssrc: Source = Source::new(Vector3::new(2.0, 1.0, 2.0), Quaternion::zero(), &room, speed_of_sound, sample_rate, None, Some(&listener));
+    let ssrc2: Source = Source::new(Vector3::new(1.0, 2.0, 3.0), Quaternion::zero(), &room, speed_of_sound, sample_rate, None, Some(&listener));
+
+    let src_tree = create_source_tree(vec![ssrc, ssrc2], &room, ism_order);
+
+    for i in src_tree.node_lists[0].iter().enumerate() {
+        println!("Nr.: {}, {:?}", i.0, src_tree.arenas[0].get(*i.1).unwrap().get().position);
+    }
+    for i in src_tree.node_lists[1].iter().enumerate() {
+        println!("Nr.: {}, {:?}", i.0, src_tree.arenas[1].get(*i.1).unwrap().get().position);
+    }
+}
+#[test]
+fn test_ism_tree_update() {
+    let ism_order: usize = 4;
+    let speed_of_sound: f32 = 343.0;
+    let sample_rate: f32 = 48000.0;
+    let room: Room = Room::new(4.0, 3.0, 5.0);
+    let listener = Listener::default();
+    let ssrc: Source = Source::new(Vector3::new(2.0, 1.0, 2.0), Quaternion::zero(), &room, speed_of_sound, sample_rate, None, Some(&listener));
+    let ssrc2: Source = Source::new(Vector3::new(1.0, 2.0, 3.0), Quaternion::zero(), &room, speed_of_sound, sample_rate, None, Some(&listener));
+
+    let mut src_tree = create_source_tree(vec![ssrc], &room, ism_order);
+    for i in src_tree.node_lists[0].iter().enumerate() {
+        println!("Nr.: {}, {:?}", i.0, src_tree.arenas[0].get(*i.1).unwrap().get().position);
+    }
+    update_source_tree(&mut src_tree, vec![ssrc2], &room, ism_order);
+    
+    for i in src_tree.node_lists[0].iter().enumerate() {
+        println!("Nr.: {}, {:?}", i.0, src_tree.arenas[0].get(*i.1).unwrap().get().position);
     }
 }
 
@@ -318,3 +487,18 @@ fn test_vector3() {
     println!("{:?}", calc_distance(&v1, &v2));
 }
 
+#[test]
+fn n_is_pm(){
+    let n = is_per_model(1, 6); 
+    println!("N of sources: {}", n);
+}
+#[test]
+fn test_vec_and_table() {
+    let mut v = Vec::new();
+    print!("Length of Vec: {}", v.len());
+    v.push(1); 
+    v.push(1); 
+    v.push(1); 
+    v.push(1); 
+    print!("Length of Vec: {}", v.len());
+}
